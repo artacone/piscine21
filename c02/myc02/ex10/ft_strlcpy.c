e4c637cbe7dddcbea7895f7d3a9a1a959b17fdfd
artacone <artacone@gmail.com>
2021-02-16 00:53:25 +0300

    optimization

--- c02/myc02/ex10/ft_strlcpy.c
+++ c02/myc02/ex10/ft_strlcpy.c
@@ -6,36 +6,34 @@
 /*   By: rvertie <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/23 19:19:51 by rvertie           #+#    #+#             */
-/*   Updated: 2021/02/16 00:24:11 by rvertie          ###   ########.fr       */
+/*   Updated: 2021/02/16 00:52:02 by rvertie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
-{
-	char	*s;
-
-	s = str;
-	while (*s)
-	{
-		s++;
-	}
-	return (s - str);
-}
-
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	unsigned int	i;
+	char			*d;
+	char			*s;
+	unsigned int	n_left;
 
-	i = 0;
-	if (size == 0)
+	d = dest;
+	s = src;
+	n_left = size;
+	if (n_left != 0)
 	{
-		return (ft_strlen(src));
+		while (--n_left != 0)
+		{
+			*d++ = *s;
+			if (*s++ == '\0')
+				break ;
+		}
 	}
-	while (i < (size - 1) && src[i])
+	if (n_left == 0)
 	{
-		dest[i] = src[i];
-		i++;
+		if (size != 0)
+			*d = '\0';
+		while (*s)
+			s++;
 	}
-	dest[i] = '\0';
-	return (ft_strlen(src));
+	return (s - src - 1);
 }
