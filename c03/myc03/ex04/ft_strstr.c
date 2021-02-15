89ba9902d70f00418795516b442484bae708a361
artacone <artacone@gmail.com>
2021-02-16 02:54:08 +0300

    add different verison (needs optimization)

--- c03/myc03/ex04/ft_strstr.c
+++ c03/myc03/ex04/ft_strstr.c
@@ -6,32 +6,65 @@
 /*   By: rvertie <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 19:57:10 by rvertie           #+#    #+#             */
-/*   Updated: 2021/01/24 20:36:18 by rvertie          ###   ########.fr       */
+/*   Updated: 2021/02/16 02:51:13 by rvertie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char		*ft_strstr(char *str, char *to_find)
+int	ft_strlen(char *str)
 {
-	int i;
-	int j;
+	char	*s;
 
-	if (!*to_find)
+	s = str;
+	while (*s)
+	{
+		s++;
+	}
+	return (s - str);
+}
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	char	*str1;
+	char	*str2;
+
+	str1 = s1;
+	str2 = s2;
+	while (n)
+	{
+		if (*str1 != *str2++)
+		{
+			return (*(unsigned char *)str1 - *(unsigned char *)(str2 - 1));
+		}
+		if (*str1++ == '\0')
+		{
+			break ;
+		}
+		n--;
+	}
+	return (0);
+}
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	char	*s;
+	int		len;
+
+	if (*to_find == '\0')
 	{
 		return (str);
 	}
-	i = 0;
-	while (str[i])
+	s = str;
+	len = ft_strlen(to_find);
+	while (*s)
 	{
-		j = 0;
-		while (str[i + j] == to_find[j])
+		while (*s != *to_find)
 		{
-			if (!to_find[j + 1])
-			{
-				return (str + i);
-			}
-			j++;
+			if (*s++ == '\0')
+				return ((char *) 0);
 		}
-		i++;
+		if (ft_strncmp(s, to_find, len) == 0)
+			return (s);
+		s++;
 	}
-	return (0);
+	return ((char *) 0);
 }
