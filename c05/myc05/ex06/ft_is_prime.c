e47c52c4388a8c9d65d234876f3b54551333326f
artacone <artacone@gmail.com>
2021-02-17 02:15:23 +0300

    optimize school method

--- c05/myc05/ex06/ft_is_prime.c
+++ c05/myc05/ex06/ft_is_prime.c
@@ -6,26 +6,34 @@
 /*   By: rvertie <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 13:30:48 by rvertie           #+#    #+#             */
-/*   Updated: 2021/02/17 01:46:24 by rvertie          ###   ########.fr       */
+/*   Updated: 2021/02/17 01:58:32 by rvertie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_is_prime(int nb)
 {
-	int	i;
+	unsigned int	i;
 
 	if (nb < 2)
 	{
 		return (0);
 	}
-	i = 2;
-	while (i < nb)
+	if (nb < 4)
 	{
-		if (nb % i == 0)
+		return (1);
+	}
+	if (nb % 2 == 0 || nb % 3 == 0)
+	{
+		return (0);
+	}
+	i = 5;
+	while (i * i <= (unsigned long)nb)
+	{
+		if (nb % i == 0 || nb % (i + 2) == 0)
 		{
 			return (0);
 		}
-		i++;
+		i += 6;
 	}
 	return (1);
 }
