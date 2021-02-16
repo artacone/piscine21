c1bd3352d54ae6b63e9c10202f62652efc6c1f63
artacone <artacone@gmail.com>
2021-02-17 02:52:54 +0300

    optimize school method

--- c05/myc05/ex07/ft_find_next_prime.c
+++ c05/myc05/ex07/ft_find_next_prime.c
@@ -6,35 +6,51 @@
 /*   By: rvertie <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 13:43:45 by rvertie           #+#    #+#             */
-/*   Updated: 2021/01/26 13:50:37 by rvertie          ###   ########.fr       */
+/*   Updated: 2021/02/17 02:51:48 by rvertie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int			ft_is_prime(int nb)
+int	ft_is_prime(int nb)
 {
-	int i;
+	unsigned int	i;
 
-	i = 2;
 	if (nb < 2)
 	{
 		return (0);
 	}
-	while (i * i <= nb)
+	if (nb < 4)
 	{
-		if (nb % i == 0)
+		return (1);
+	}
+	if (nb % 2 == 0 || nb % 3 == 0)
+	{
+		return (0);
+	}
+	i = 5;
+	while (i * i <= (unsigned long)nb)
+	{
+		if (nb % i == 0 || nb % (i + 2) == 0)
 		{
 			return (0);
 		}
-		i++;
+		i += 6;
 	}
 	return (1);
 }
 
-int			ft_find_next_prime(int nb)
+int	ft_find_next_prime(int nb)
 {
+	if (nb <= 2)
+	{
+		return (2);
+	}
+	if (!(nb & 1))
+	{
+		++nb;
+	}
 	while (!ft_is_prime(nb))
 	{
-		nb++;
+		nb += 2;
 	}
 	return (nb);
 }
