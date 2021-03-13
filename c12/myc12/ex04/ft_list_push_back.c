d7781f596a53a1f446c247baaca3211558e01ddb
artacone <artacone@gmail.com>
2021-03-13 19:25:58 +0300

    norm

--- c12/myc12/ex04/ft_list_push_back.c
+++ c12/myc12/ex04/ft_list_push_back.c
@@ -6,15 +6,15 @@
 /*   By: rvertie <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 00:10:12 by rvertie           #+#    #+#             */
-/*   Updated: 2021/02/05 21:26:47 by rvertie          ###   ########.fr       */
+/*   Updated: 2021/03/13 19:17:54 by rvertie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
-t_list		*ft_list_last(t_list *begin_list)
+t_list	*ft_list_last(t_list *begin_list)
 {
-	t_list *tmp;
+	t_list	*tmp;
 
 	tmp = begin_list;
 	if (tmp)
@@ -27,9 +27,9 @@ t_list		*ft_list_last(t_list *begin_list)
 	return (tmp);
 }
 
-void		ft_list_push_back(t_list **begin_list, void *data)
+void	ft_list_push_back(t_list **begin_list, void *data)
 {
-	t_list *last;
+	t_list	*last;
 
 	last = *begin_list;
 	if (last)
