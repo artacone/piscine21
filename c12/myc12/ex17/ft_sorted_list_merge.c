b31534df07697917c880c37c9a755af4d1882f52
artacone <artacone@gmail.com>
2021-03-13 23:36:11 +0300

    fix bug

--- c12/myc12/ex17/ft_sorted_list_merge.c
+++ c12/myc12/ex17/ft_sorted_list_merge.c
@@ -6,35 +6,35 @@
 /*   By: rvertie <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 23:33:49 by rvertie           #+#    #+#             */
-/*   Updated: 2021/02/04 23:37:59 by rvertie          ###   ########.fr       */
+/*   Updated: 2021/03/13 23:35:06 by rvertie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
-t_list		*ft_sorted_merge(t_list *l, t_list *r, int (*cmp)())
+t_list	*ft_sorted_merge(t_list *l, t_list *r, int (*cmp)())
 {
-	t_list *result;
+	t_list	*result;
 
 	result = NULL;
 	if (l == NULL)
 		return (r);
 	else if (r == NULL)
 		return (l);
-	if ((*cmp)(l->data, r->data))
+	if ((*cmp)(l->data, r->data) <= 0)
 	{
 		result = l;
-		result->next = ft_sorted_merge(l->next, r);
+		result->next = ft_sorted_merge(l->next, r, cmp);
 	}
 	else
 	{
 		result = r;
-		result->next = ft_sorted_merge(l, r->next);
+		result->next = ft_sorted_merge(l, r->next, cmp);
 	}
 	return (result);
 }
 
-void		ft_sorted_list_merge(t_list **begin_list1,
+void	ft_sorted_list_merge(t_list **begin_list1,
 		t_list *begin_list2, int (*cmp)())
 {
 	*begin_list1 = ft_sorted_list_merge(*begin_list1, begin_list2, cmp);
