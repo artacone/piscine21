6aec82dfab2290e21f237ec4b1d804cc7853a0d1
artacone <artacone@gmail.com>
2021-03-13 23:05:48 +0300

    add iterative algorithm

--- c12/myc12/ex15/ft_list_reverse_fun.c
+++ c12/myc12/ex15/ft_list_reverse_fun.c
@@ -6,15 +6,34 @@
 /*   By: rvertie <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 23:23:46 by rvertie           #+#    #+#             */
-/*   Updated: 2021/02/05 21:27:41 by rvertie          ###   ########.fr       */
+/*   Updated: 2021/03/13 23:05:01 by rvertie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
-t_list		*reverse(t_list *head)
+t_list	*reverse(t_list **head_ref)
 {
-	t_list *rest;
+    t_list	*prev;
+    t_list	*current;
+    t_list	*next;
+
+	prev = NULL;
+	current = *head_ref;
+	next = NULL;
+    while (current != NULL)
+	{
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    *head_ref = prev;
+}
+/*
+t_list	*reverse(t_list *head)
+{
+	t_list	*rest;
 
 	if (head == NULL || head->next == NULL)
 	{
@@ -25,8 +44,9 @@ t_list		*reverse(t_list *head)
 	head->next = NULL;
 	return (rest);
 }
+*/
 
-void		ft_list_reverse_fun(t_list *begin_list)
+void	ft_list_reverse_fun(t_list *begin_list)
 {
-	begin_list = reverse(begin_list);
+	begin_list = reverse(&begin_list);
 }
